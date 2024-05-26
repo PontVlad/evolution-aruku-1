8b6e33f39805744b4bae6367eafa041091d57ad2
PontVlad <128230225+PontVlad@users.noreply.github.com>
2024-05-26 08:40:17 +0300

    Add files via upload

---
+++ server/routes/oauth/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+import {server$oauthVKRegister} from '../../actions/oauth';
+
+
+router.get('/vk', function (req, res, next) {
+  req.app.get('store').dispatch(server$oauthVKRegister(req.headers['x-forwarded-proto'], req.headers.host, req.query.code))
+  // .then((token) => {
+  //   console.log('redirecting')
+  //   console.log('/login?token=' + token)
+  //   return token;
+  // })
+    .then((token) => res.redirect('/login?token=' + token))
+    .catch(err => res.redirect('/login?error=' + err));
+});
+
+export default router;
